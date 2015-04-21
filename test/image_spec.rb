2e4d78042c6cc29e90fa4ea2a7b7c91228339780
Timothy Nordloh <tnordloh@gmail.com>
2015-04-20 20:37:14 -0600

    rename spec to test

--- test/image_spec.rb
+++ test/image_spec.rb
@@ -6,19 +6,19 @@ describe ImageMagick::Image do
   it "gets file attributes" do
     test_image =  File.dirname(__FILE__) + "/data/black_white_flower.tif"
     i = ImageMagick::Image.new(test_image)
-    i.name.to_s.must_equal("./spec/data/black_white_flower.tif")
+    i.name.to_s.must_equal("./test/data/black_white_flower.tif")
     i.shortname.to_s.must_equal("black_white_flower")
     i.width.must_equal(1950)
     i.length.must_equal(2722)
     i.size.must_equal(9.998)
-    i.output_dir.must_equal("./spec/data/out")
+    i.output_dir.must_equal("./test/data/out")
   end
 
   it "compresses an image" do
     test_image =  File.dirname(__FILE__) + "/data/black_white_flower.tif"
     i = ImageMagick::Image.new(test_image)
     new_image = i.compress
-    path = "./spec/data/out/black_white_flower.tif"
+    path = "./test/data/out/black_white_flower.tif"
     new_image.name.to_s.must_equal(path)
     File.delete(new_image.name)
   end
